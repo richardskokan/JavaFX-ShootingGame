25b3950107b352ed6e6ec917f001ec9493306b7d
Richard Skokan <skokan@spse-po.sk>
2021-02-02 23:21:14 +0100

    Moved main.fxml and added jFoenix dependencies so it works

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,7 +7,7 @@ import javafx.stage.Stage;
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/Resources/main.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/layouts/main.fxml"));
         primaryStage.setTitle("Shooting Game");
         primaryStage.setScene(new Scene(root));
         primaryStage.setResizable(false);
