0f3a27769b6024d562b0aa9dff3955023166b52a
Richard Skokan <skokan@spse-po.sk>
2021-02-09 20:32:08 +0100

    Custom object Target (forgot to add it with previous commit)

---
+++ src/main/java/Objects/Target.java
@@ -0,0 +1,49 @@
+package Objects;
+
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+
+public class Target {
+    Circle innerTarget = new Circle(40);
+    Circle outerTarget = new Circle(80);
+
+    public Target(double x, double y) {
+        innerTarget.setFill(new Color(0.212, 0.212, 0.212, 1));
+        innerTarget.setStroke(new Color(0, 0, 0, 1));
+        innerTarget.setStrokeWidth(2);
+
+        outerTarget.setFill(new Color(0.275, 0.275, 0.275, 1));
+        outerTarget.setStroke(new Color(0, 0, 0, 1));
+        outerTarget.setStrokeWidth(2);
+
+        innerTarget.setLayoutX(x);
+        innerTarget.setLayoutY(y);
+
+        outerTarget.setLayoutX(x);
+        outerTarget.setLayoutY(y);
+    }
+
+    public Circle getInnerTarget() {
+        return innerTarget;
+    }
+
+    public Circle getOuterTarget() {
+        return outerTarget;
+    }
+
+    public void targetHit() {
+        innerTarget.setFill(new Color(1, 1, 1, 1));
+        innerTarget.setStroke(new Color(1, 1, 1, 1));
+        innerTarget.setStrokeWidth(2);
+
+        outerTarget.setFill(new Color(1, 1, 1, 1));
+    }
+
+    public void resetTarget() {
+        innerTarget.setFill(new Color(0.212, 0.212, 0.212, 1));
+        innerTarget.setStroke(new Color(0, 0, 0, 1));
+        innerTarget.setStrokeWidth(2);
+
+        outerTarget.setFill(new Color(0.275, 0.275, 0.275, 1));
+    }
+}
