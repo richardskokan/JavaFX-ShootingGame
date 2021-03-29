21c6f2cc55df13d7f8979db0680639312b859681
Richard Skokan <skokan@spse-po.sk>
2021-03-29 19:39:21 +0200

    Added controller and .fxml for "Top Scores" window, added its launch method as onAction for "Skóre" button.

---
+++ src/main/java/Controllers/ScoreController.java
@@ -0,0 +1,14 @@
+package Controllers;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+//TODO
+public class ScoreController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
